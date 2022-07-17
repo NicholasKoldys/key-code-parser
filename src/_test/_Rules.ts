88fa1da1d0055deae5c4cc5498cae29663e24fcd
NicholasKoldys <nicholas.koldys@gmail.com>
2022-07-17 23:23:49 +0700

    add formating to rules test

--- src/_test/_Rules.ts
+++ src/_test/_Rules.ts
@@ -18,28 +18,35 @@ const CustomKeys = Object.assign( Object.assign({}, defaultKeys), {
 });
 
 TestsOf("Testing Keys String Building: ", {
-  'TestKey created      ': 
+
+  'TestKey created              ': 
     [ () => assertEquals( TestKey, '\`{3}' ), 'reading default keys properly'],
-  'User Keys inheriting ': 
+
+  'User Keys inheriting         ': 
     [ () => assertEquals( CustomKeys[Keyable.Fencing], defaultKeys[Keyable.Fencing] ), 'testing custom user keys.'],
-  'FailIf overwriting   ': 
+
+  'FAILIF overwriting           ': 
     [ () => assertEquals( CustomKeys[Keyable.HeaderPrimaryKey], defaultKeys[Keyable.HeaderPrimaryKey], true ), 'testing custom user keys.'],
 });
 
 TestsOf("Testing Keys String Building: ", {
-  'TestKey created      ': 
+
+  'TestKey created              ': 
     [ () => assertEquals( TestKey, '\`{3}' ), 'reading default keys properly'],
-  'User Keys inheriting ': 
+
+  'User Keys inheriting         ': 
     [ () => assertEquals( CustomKeys[Keyable.Fencing], defaultKeys[Keyable.Fencing] ), 'testing custom user keys.'],
-  'FailIf overwriting   ': 
+
+  'FAILIF overwriting           ': 
     [ () => assertEquals( CustomKeys[Keyable.HeaderPrimaryKey], defaultKeys[Keyable.HeaderPrimaryKey], true ), 'as user specified keys do not allow mutliple keys for single-Keyable.'],
 });
 
 const BuiltBlockRules = BlockOrderedRules( defaultKeys);
 const BuiltInlineRules = InlineOrderedRules( defaultKeys);
 
-TestsOf("Testing Block Rules: ", {
-  'Sanity Space                       ':
+TestsOf("Sanity Checking with spaces: ", {
+
+  'Sanity Space                 ':
     [ () => {
       const newline1 = '\n';
       const newline2 = "\n";
@@ -50,14 +57,16 @@ TestsOf("Testing Block Rules: ", {
       assertEquals( newline2, newline3 );
       assertEquals( newline3, newline4 );
     }, 'ensured spaceing in my mind works.'],
-  'FailIf Templates aren`t checked    ':
+
+  'FAILIF templated str spaces  ':
     [ () => {
       const newline1 = '\n';
       const newline4 = `
       `;
       assertEquals( newline1, newline4, true );
     }, 'to check template spacing as the editor space is considered with templates.'],
-  'Allowable space                    ': 
+
+  'Allowable space              ': 
     [ () => {
       const allspaces = '         ';
       const newline = `
@@ -65,7 +74,8 @@ TestsOf("Testing Block Rules: ", {
       assertEquals( BuiltBlockRules['AllowableSpace'].regex.exec(allspaces)?.groups?.RESULT, allspaces )
       assertEquals( BuiltBlockRules['AllowableSpace'].regex.exec(newline)?.groups?.RESULT, "\n" )
     }, 'has regex'],
-  'FailIf more than oneline of spaces ': 
+
+  'FAILIF multi newlines        ': 
     [ () => {
       const allspaces = '         \n\n';
       const newline = `\n
@@ -73,7 +83,11 @@ TestsOf("Testing Block Rules: ", {
       assertEquals( BuiltBlockRules['AllowableSpace'].regex.exec(allspaces)?.groups?.RESULT, allspaces, true )
       assertEquals( BuiltBlockRules['AllowableSpace'].regex.exec(newline)?.groups?.RESULT, "\n\n", true )
     }, 'to get multi newlines \\n\\n and template multiline two newlines.'],
-  'Fencing captures all within keys   ':
+});
+
+TestsOf("Block Rules: ", {
+
+  'Fencing captures between     ':
     [ () => {
       const fencingStringNull = "``````";
       const fencingStringX = "```x```";
@@ -82,16 +96,18 @@ TestsOf("Testing Block Rules: ", {
       assertEquals( BuiltBlockRules['Fencing'].regex.exec( fencingStringX )?.groups?.RESULT, 'x' );
       assertEquals( BuiltBlockRules['Fencing'].regex.exec( fencingStringWithSpaces )?.groups?.RESULT, 'x\nx    \nxxxx     \n\nx' );
     }, 'allows newlines and all capturable characters.'],
-  'HeadingSection captures all under  ':
+
+  'HeadingSection captures under':
     [ () => {
       const headingSectionKeyOut = "Heading 1: Title of Section\n---"
       const headerOnlyCapture = "Heading 1: Title of Section\n---\n\n continuing text after.. ";
-      assertEquals( BuiltBlockRules['Heading1Sect'].regex.exec( headingSectionKeyOut )?.groups?.RESULT, 
+      assertEquals( BuiltBlockRules['HeadingSect'].regex.exec( headingSectionKeyOut )?.groups?.RESULT, 
         'Heading 1: Title of Section\n' );
-      assertEquals( BuiltBlockRules['Heading1Sect'].regex.exec( headerOnlyCapture )?.groups?.RESULT, 
+      assertEquals( BuiltBlockRules['HeadingSect'].regex.exec( headerOnlyCapture )?.groups?.RESULT, 
         'Heading 1: Title of Section\n' );
     }, 'only selecting the header text.'],
-  'Text groups everything else        ':
+
+  'Text captures multilines     ':
     [ () => {
       const singleLine = "Text with no starting newline find me @outside.com\n"
       const preserveSpacing = "\t    \t  spacing and text with a single newline are captured \n  .... just not the ending the next.... ";
@@ -100,16 +116,18 @@ TestsOf("Testing Block Rules: ", {
       assertEquals( BuiltBlockRules['Text'].regex.exec( preserveSpacing )?.groups?.RESULT, 
         '\t    \t  spacing and text with a single newline are captured \n' );
     }, 'only selecting a single line of text.'],
-});
+})
 
-TestsOf("Testing Inline Rules: ", {
-  'Paragraph       ':
+TestsOf("Inline Rules: Paragraph - ", {
+
+  'single-line                  ':
     [ () => {
       const justParagraph = "This is some text without a newline as the newline wont be taken.";
       assertEquals( BuiltInlineRules['Paragraph'].regex.exec( justParagraph )?.groups?.RESULT, 
         justParagraph );
     }, 'paragraph line.'],
-  'Paragraph captures multi-lines':
+
+  'captures multi-lines         ':
     [ () => {
       const pWithNewline = "This is some text with a newline\n continued";
       const pWithMultilines = "This is some \ntext with some newlines\n \n hello!";
@@ -121,49 +139,59 @@ TestsOf("Testing Inline Rules: ", {
       assertEquals( BuiltInlineRules['Paragraph'].regex.exec( pWithPreSpacing )?.groups?.RESULT, 
         pWithPreSpacing );
     }, 'grab space to preserve.'],
-  'Paragraph FAILIF expecting highlighted/span':
+
+  'FAILIF expecting high/span   ':
     [ () => {
       const pWithMultilines = "This is some highlighted \`\`text with\n a newline\`\`!";
       assertEquals( BuiltInlineRules['Paragraph'].regex.exec( pWithMultilines )?.groups?.RESULT, 
         pWithMultilines, true );
     }, 'only expect pre-highlight.'],
-  'Paragraph expecting pre- highlighted/span':
+
+  'expecting pre- high/span     ':
     [ () => {
       const pWithMultilines = "This is some highlighted \`\`text with\n a newline\`\`!";
       assertEquals( BuiltInlineRules['Paragraph'].regex.exec( pWithMultilines )?.groups?.RESULT, 
         'This is some highlighted ' );
-    }, 'contains text.'],
-  'Highlight captures RESULT':
+    }, 'paragraph cutting out other keys.'],
+});
+
+TestsOf("Inline Rules: Highlight - ", {
+  
+  'captures RESULT              ':
     [ () => {
-      const highlighted = "\`\`highlightedText\`\`"
-      const highlightedResult = "\`\`~thisType highlightedResult\`\`";
-      assertEquals( BuiltInlineRules['Highlight'].regex.exec( highlighted )?.groups?.RESULT, 
-        'highlightedText' );
-      assertEquals( BuiltInlineRules['Highlight'].regex.exec( highlightedResult )?.groups?.RESULT, 
-        'highlightedResult' );
+      const highlightDefault = "\`\`highlighted Text - Hello\`\`"
+      const highlightWType = "\`\`~thisType highlighted Result! ~  \`\`";
+      assertEquals( BuiltInlineRules['Highlight'].regex.exec( highlightDefault )?.groups?.RESULT, 
+        'highlighted Text - Hello' );
+      assertEquals( BuiltInlineRules['Highlight'].regex.exec( highlightWType )?.groups?.RESULT, 
+        'highlighted Result! ~  ' );
     }, 'contains text.'],
-  'Highlight captures TYPE':
+
+  'captures TYPE                ':
     [ () => {
       const highlightedResult = "\`\`~thisType highlightedResult\`\`";      
       assertEquals( BuiltInlineRules['Highlight'].regex.exec( highlightedResult )?.groups?.TYPE, 
         'thisType' );
     }, 'contains text.'],
-  'Spannable captures RESULT':
-    [ () => {
-      const spanBoldResult = "**spannable Bolded result**";
-      const spanRedactResult = "~~spannable Redacted result~~";
-      assertEquals( BuiltInlineRules['Span'].regex.exec( spanBoldResult )?.groups?.RESULT, 
-        'spannable Bolded result' );
-      assertEquals( BuiltInlineRules['Span'].regex.exec( spanRedactResult )?.groups?.RESULT, 
-        'spannable Redacted result' );
-    }, 'types are read.'],
-  'Spannable captures TYPE':
-    [ () => {
-      const spanBoldResult = "**spannable result**";
-      const spanRedactResult = "~~spannable result~~";
-      assertEquals( BuiltInlineRules['Span'].regex.exec( spanBoldResult )?.groups?.TYPE, 
-        '**' );
-      assertEquals( BuiltInlineRules['Span'].regex.exec( spanRedactResult )?.groups?.TYPE, 
-        '~~' );
-    }, 'types are read.'],
 });
+
+TestsOf("Spannable Rules: Span - ", {
+  'Spannable captures RESULT    ':
+  [ () => {
+    const spanBoldResult = "**spannable Bolded result**";
+    const spanRedactResult = "~~spannable Redacted result~~";
+    assertEquals( BuiltInlineRules['Span'].regex.exec( spanBoldResult )?.groups?.RESULT, 
+      'spannable Bolded result' );
+    assertEquals( BuiltInlineRules['Span'].regex.exec( spanRedactResult )?.groups?.RESULT, 
+      'spannable Redacted result' );
+  }, 'types are read.'],
+  'Spannable captures TYPE      ':
+  [ () => {
+    const spanBoldResult = "**spannable result**";
+    const spanRedactResult = "~~spannable result~~";
+    assertEquals( BuiltInlineRules['Span'].regex.exec( spanBoldResult )?.groups?.TYPE, 
+      '**' );
+    assertEquals( BuiltInlineRules['Span'].regex.exec( spanRedactResult )?.groups?.TYPE, 
+      '~~' );
+  }, 'types are read.'],
+})
