1ded2fb4068e76c5f2a9ab27527821565224207f
NicholasKoldys <nicholas.koldys@gmail.com>
2022-07-26 18:33:00 +0700

    just ignore it..

--- src/_test/test-lib.test.ts
+++ src/_test/test-lib.test.ts
@@ -2,7 +2,8 @@ import { TestsOf, assert, assertEquals } from "./test-lib.js";
 
 const LibraryTest = TestsOf( "TestSectionTitle: ", {
   'Assert w equals': [() => assert(2.6 == 2.6), 'assert is checking'],
-  'Assert w !equal': [() => assert(undefined == 1), 'assert is Failing'],
+ // @ts-ignore
+  'Assert w !equal': [() => assert(0 == 1), 'assert is Failing'],
   'Assert Solution': [() => assertEquals(9, 3*3), 'assertEquals is checking'],
   'Assert Wrong ==': [() => assertEquals(null, 1), 'assertEquals is Failing']
 });
