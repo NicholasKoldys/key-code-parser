2f8c876b50ff090778888d86f6679029fab17b4a
NicholasKoldys <nicholas.koldys@gmail.com>
2022-08-06 23:01:16 +0700

    add media; add webpack bundler

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+import { DeclarationWebpackPlugin, moduleType } from '@nicholaskoldys/declaration-webpack-plugin/DeclarationWebpackPlugin.min.js';
+import { resolve } from 'node:path';
+
+const __dirname = import.meta.dirname;
+
+const webpackConfig = {
+  mode: 'production',
+  entry: './src/Parser.ts',
+  experiments: {
+    outputModule: true,
+  },
+
+  output: {
+    filename: 'KeyCodeParser.min.js',
+    library: {
+      type: 'modern-module',
+    },
+    path: resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  infrastructureLogging: {
+    level: 'verbose',
+  },
+
+  resolve: {
+    extensions: [ '.ts', '.js' ],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?ts$/,
+        use: [ 
+          { 
+            loader: 'ts-loader', 
+            options: { 
+              configFile: resolve(__dirname, './src/tsconfig.dist.json') 
+            }
+          },
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new DeclarationWebpackPlugin({
+      moduleName: "KeyCodeParser",
+      moduleType: moduleType.esm,
+      outputFile:'KeyCodeParser.d.ts',
+      isFileCommented: true,
+    })
+  ],
+};
+
+export default webpackConfig;
